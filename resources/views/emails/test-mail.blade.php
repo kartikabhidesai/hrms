2dcb8236e99e8081432b659c7243d97ed4b4b315
kartik <kartikdesai123@gmail.com>
2019-01-26 01:16:33 +0530

    dd

--- resources/views/emails/test-mail.blade.php
+++ resources/views/emails/test-mail.blade.php
@@ -1,51 +1,122 @@
-<div style="width:100%; margin:0px; padding: 0px;">
-    <!-- main -->
-    <div style="padding: 48px;">
-        <div style="width: 560px; margin: 0 auto; display: block; box-shadow: 0px 0px 14px 0px rgba(142, 140, 140, 0.67); background: #fff; padding: 48px;">
-            <!-- wrapper -->
-            <p style="">
-                <img src="<?php echo url('img/mvc_logo.png'); ?>" style="display: block;float: left;width: 50px;margin-right: 20px;">
-                <h1>Forgot password</h1>
-            </p>
-            
-            <h2 style="padding: 26px 0 0px 7px;text-align: center;"> Please login with new password. remember it.</h2>
-            <div style="margin-top: 78px; box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.33); padding: 16px;">
-                <table>
-                    <tr>
-                        <th colspan="3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
-                    </tr>
-                    <tr>
-                        <td>Hello </td>
-                        <th>{{ $data['name'] }}</th>
-                    </tr> 
-                    <tr>
-                        <td>Caller Email : </td>
-                        <th>{{ $data['caller_email'] }}</th>
-                    </tr>
-                    <tr>
-                        <td>New Password : </td>
-                        <th>{{ $data['password'] }}</th>
-                    </tr>
-                </table>
-                <p>Kind Regards</p>
-                <p>Office Park Team</p>
-            </div>
-            <!-- content over -->
-        </div>
-        <!-- wrapper over -->
-    </div>
-</div>
-<style>
-    table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-    }
+<!doctype html>
+<html>
+<head>
+<title></title>
+<style type="text/css">
+/* CLIENT-SPECIFIC STYLES */
+body, table, td, a { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+table, td { mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+img { -ms-interpolation-mode: bicubic; }
 
-    td, th {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-    }
+/* RESET STYLES */
+img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; }
+table { border-collapse: collapse !important; }
+body { height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important; }
 
+/* iOS BLUE LINKS */
+a[x-apple-data-detectors] {
+   color: inherit !important;
+   text-decoration: none !important;
+   font-size: inherit !important;
+   font-family: inherit !important;
+   font-weight: inherit !important;
+   line-height: inherit !important;
+}
+
+/* MOBILE STYLES */
+@media screen and (max-width: 600px) {
+ .img-max {
+   width: 100% !important;
+   max-width: 100% !important;
+   height: auto !important;
+ }
+
+ .max-width {
+   max-width: 100% !important;
+ }
+
+ .mobile-wrapper {
+   width: 85% !important;
+   max-width: 85% !important;
+ }
+
+ .mobile-padding {
+   padding-left: 5% !important;
+   padding-right: 5% !important;
+ }
+}
+
+/* ANDROID CENTER FIX */
+div[style*="margin: 16px 0;"] { margin: 0 !important; }
 </style>
+</head>
+<body style="margin: 0 !important; padding: 0; !important background-color: #F1F1F1;" bgcolor="#F1F1F1">
+
+<!-- HIDDEN PREHEADER TEXT -->
+<div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Open Sans, Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+   Herzlich Willkommen bei Office Park Telefonservice. 
+</div>
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+   <tr>
+       <td align="center" valign="top" width="100%" bgcolor="#FFFFFF" style="background: #F9F9F9 ; background-size: cover; background-attachment: fixed; padding: 50px 15px 0 15px;" class="mobile-padding">
+           <!--[if (gte mso 9)|(IE)]>
+           <table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
+           <tr>
+           <td align="center" valign="top" width="600">
+           <![endif]-->
+           <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+               <tr>
+                   <td align="center" valign="top" style="padding: 0 0 50px 0;">
+                       <img src="<?php echo url('img/mvc_logo.png'); ?>" width="250" height="145" border="0" style="display: block;">
+                   </td>
+               </tr>
+               <tr>
+                   <td align="left" valign="top" style="padding: 50px; font-family: Open Sans, Helvetica, Arial, sans-serif; border-radius: 3px; rgba(0,0,0,.5);" bgcolor="#ffffff">
+                       <p style="color: #999999; font-size: 16px; line-height: 26px; margin: 0;">
+
+                           Sehr {{ $data['name'] }},<br><br>
+
+                          wir freuen uns, dass wir Sie als neuen OFFICE PARK-Kunden begrüßen dürfen. Ihr Erreichbarkeitsservice ist ab sofort einsatzbereit und unsere Agenten nehmen Ihre Anrufe entgegen. Mit diesem Schreiben erhalten Sie eine Übersicht über Ihre Kundendaten, unsere AGB‘s, die Preisliste sowie Hinweise für Ihren persönlichen Kundenbereich.<br><br>
+                           
+                          Bitte nehmen Sie sich am Anfang etwas Zeit um alle Daten zu kontrollieren und ggf. zu ergänzen! Weiterhin bitten wir Sie, uns die beiliegende Sepa-Lastschrift-Verfügung unterschrieben zurückzusenden - gerne auch per E-Mail.<br><br>
+                          
+                          
+                           Wir wünschen Ihnen viel Erfolg mit Ihrem neuen Telefonsekretariat und freuen uns auf eine gute und langjährige Zusammenarbeit. Sollten Sie weitere Fragen haben, stehen Ihnen unsere Mitarbeiter aus dem Service-Team gerne telefonisch unter 0211 368 74 190 zur Verfügung.<br><br>
+
+                            Mit freundlichen Grüßen,<br>
+                           <img src="images/signatur.png" width="130" height="168" style="display: block; border: 0px;"/>
+                           Baris Ak <br>
+Geschäftsführer<br>
+                       </p>
+                   </td>
+               </tr>
+               <tr>
+                   <td align="center" valign="top" style="padding: 25px 0; font-family: Open Sans, Helvetica, Arial, sans-serif; color: #000;">
+                       <p style="font-size: 14px; line-height: 20px;color:#999999">
+                          Office Park GbR<br>
+                          Münsterstr. 330<br>
+                          40470 Düsseldorf
+
+                         <br><br>
+
+                    
+                         <a href="#" style="color: #999999;" target="_blank">Datenschutz </a>
+                         &nbsp; &bull; &nbsp;
+                         <a href="#" style="color: #999999;" target="_blank">Impressum</a>
+                         &nbsp; &bull; &nbsp;
+                         <a href="#" style="color: #999999;" target="_blank">Kontakt</a>
+                       </p>
+                   </td>
+               </tr>
+           </table>
+           <!--[if (gte mso 9)|(IE)]>
+           </td>
+           </tr>
+           </table>
+           <![endif]-->
+       </td>
+   </tr>
+</table>
+</body>
+</html>
