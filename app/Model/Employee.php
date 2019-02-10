29572be89896cec64c8a38c8fd029f2bb68f79f7
Parth Khunt <parthkhunt12@gmail.com>
2019-02-10 15:06:44 +0530

    CHnage in Migrtion file

--- app/Model/Employee.php
+++ app/Model/Employee.php
@@ -366,7 +366,7 @@ class Employee extends Model {
     }
     
     public function getEmploydetails($userId){
-//        print_r($userId);exit;
+        print_r($userId);exit;
         $result = Employee::select('department.department_name','department.id as dep_id','employee.name','employee.company_id','employee.id as emp_id')
                     ->join('department', 'employee.department', '=', 'department.id')
                     ->where('employee.user_id',$userId)->get();
