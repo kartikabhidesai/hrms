514efbaede3e9206b634402d530c41d1d2650481
kartik <kartikdesai123@gmail.com>
2019-02-10 17:49:27 +0530

    solve

--- resources/views/company/pay-slip/create.blade.php
+++ resources/views/company/pay-slip/create.blade.php
@@ -47,7 +47,7 @@
                 <div class="form-group">
                     <label class="col-sm-2 control-label">Month</label>
                     <div class="col-sm-9">
-                        {{ Form::select('months',  array('L' => 'Large', 'S' => 'Small') , null, array('class' => 'form-control m-b', 'id' => 'test')) }}
+                        {{ Form::select('months', $monthis , null, array('class' => 'form-control m-b', 'id' => 'test')) }}
                     </div>
                 </div>
 
