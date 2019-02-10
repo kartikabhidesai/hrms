0f95407f4d04041271ef4aeb7a76608fa5533a1e
Parth Khunt <parthkhunt12@gmail.com>
2019-02-10 14:36:07 +0530

    Chnage in class name

--- database/migrations/2019_02_10_085829_add_change_tyepid_type.php
+++ database/migrations/2019_02_10_085829_add_change_tyepid_type.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddChangeTyepidType extends Migration
+class AddChangeTyepidTypeOld extends Migration
 {
     /**
      * Run the migrations.
