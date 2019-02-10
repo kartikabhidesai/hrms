f8a2e7fd1ae65a9669f50dc74fd5cfe934f409d5
Parth Khunt <parthkhunt12@gmail.com>
2019-02-10 14:40:40 +0530

    CHnage in Migrtion file

--- database/migrations/2019_02_10_090922_add_request_type_coloum_tyepid_type.php
+++ database/migrations/2019_02_10_090922_add_request_type_coloum_tyepid_type.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddChangeTyepidTypeOld extends Migration
+class AddRequestTypeColoumTyepidType extends Migration
 {
     /**
      * Run the migrations.
