977f7d0231b2e12d53564a3e74a6962c1e4a9f95
Chetan <patelchetan1801@gmail.com>
2019-02-16 12:47:58 +0530

    design change right side user listing

--- resources/views/company/communication/communication.blade.php
+++ resources/views/company/communication/communication.blade.php
@@ -5,32 +5,23 @@
         <div class="col-lg-12">
             <div class="ibox float-e-margins">
                 <div class="ibox-content">
-
                     <strong>Chat room </strong> can be used to create chat room in your app.
                     You can also use a small chat in the right corner to provide live discussion.
-
                 </div>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-12">
-
             <div class="ibox chat-view">
-
                 <div class="ibox-title">
                     <small class="pull-right text-muted">Last message:  Mon Jan 26 2015 - 18:39:23</small>
                     Chat room panel
                 </div>
-
-
                 <div class="ibox-content">
-
                     <div class="row">
-
                         <div class="col-md-9 ">
                             <div class="chat-discussion">
-
                                 <div class="chat-message left">
                                     <img class="message-avatar" src="/img/a1.jpg" alt="" >
                                     <div class="message">
@@ -83,32 +74,11 @@
                                         </span>
                                     </div>
                                 </div>
-
                             </div>
-
                         </div>
                         <div class="col-md-3">
                             <div class="chat-users">
-
-
                                 <div class="users-list">
-                                    
-                                    <
-                                    <div class="chat-user">
-                                        <span class="pull-right label label-primary">Online</span>
-                                        <img class="chat-avatar" src="/img/a2.jpg" alt="" >
-                                        <div class="chat-user-name">
-                                            <a href="#">Michael Smith</a>
-                                        </div>
-                                    </div>
-                                  
-                                    <div class="chat-user">
-                                        <span class="pull-right label label-primary">Online</span>
-                                        <img class="chat-avatar" src="//img/a3.jpg" alt="" >
-                                        <div class="chat-user-name">
-                                            <a href="#">Janet Smith</a>
-                                        </div>
-                                    </div>
                                     <?php
                                     foreach ($empArray as $key => $value) {
                                         ?>
@@ -120,36 +90,23 @@
                                         </div>
                                     <?php }
                                     ?>
-
-
                                 </div>
-
                             </div>
                         </div>
-
                     </div>
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="chat-message-form">
-
                                 <div class="form-group">
-
                                     <textarea class="form-control message-input" name="message" placeholder="Enter message text"></textarea>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-
-
                 </div>
-
             </div>
         </div>
-
     </div>
-
-
 </div>
 
 
