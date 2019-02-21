14d0e42c4834dc5b6c42f2fb56b78129f7ff8f03
shailesh <shailesh.vanaliya91@gmail.com>
2019-02-22 00:37:41 +0530

    change in add advance salary in company

--- app/Model/Advancesalary.php
+++ app/Model/Advancesalary.php
@@ -195,7 +195,7 @@ class Advancesalary extends Model
                     $actionHtml='<span class="label label-danger">Rejected</span>';
                 }
             }
-            $filePath =  '/hrms/uploads/employee/advance_salary_request/'.$row['file_name'];
+            $filePath =  '/uploads/employee/advance_salary_request/'.$row['file_name'];
 
             $nestedData = array();
               $nestedData[] = '<input type="checkbox" class="chkChangeReq" name="chkChangeReq" value="'.$row['id'].'">';
