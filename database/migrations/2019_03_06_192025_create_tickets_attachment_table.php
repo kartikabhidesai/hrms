260fc6e1d4c1ea7e20f99fa38ad9bde5105e211e
Hiren Gohel <hirengohel.004@gmail.com>
2019-03-06 22:34:00 +0530

    fixed error of migration

--- database/migrations/2019_03_06_192025_create_tickets_attachment_table.php
+++ database/migrations/2019_03_06_192025_create_tickets_attachment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTicketAttachmentsTable extends Migration
+class CreateTicketsAttachmentTable extends Migration
 {
     /**
      * Run the migrations.
