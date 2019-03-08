49fc874be17e75a78545338bd633624b5b51ed48
Ravi Gopani <ravigopani555@gmail.com>
2019-03-08 15:37:05 +0530

    Create 2019_03_08_100516_add_creted_by_in_ticket_table.php

---
+++ database/migrations/2019_03_08_100516_add_creted_by_in_ticket_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCretedByInTicketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ticket', function (Blueprint $table) {
+            $table->string('created_by')->after('details');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ticket', function (Blueprint $table) {
+            $table->string('created_by')->after('details');
+        });
+    }
+}
