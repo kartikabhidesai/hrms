9a5a36365b286035031ef145fcf4d4af75132158
shailesh <shailesh.vanaliya91@gmail.com>
2019-03-13 00:06:04 +0530

    chang in database file

--- config/database.php
+++ config/database.php
@@ -50,7 +50,16 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
+            'modes' => [
+                //'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_AUTO_CREATE_USER',
+                'NO_ENGINE_SUBSTITUTION'
+             ],
             'engine' => null,
         ],
 
