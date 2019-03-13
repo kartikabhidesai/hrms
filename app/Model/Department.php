d2b7d76bf4e50dc13185d193e8753b0ee583920d
shailesh <shailesh.vanaliya91@gmail.com>
2019-03-14 00:33:09 +0530

    change in department

--- app/Model/Department.php
+++ app/Model/Department.php
@@ -61,8 +61,7 @@ class Department extends Model
         return $arrDepartment;
     }
 
-    public function getdatatable()
-    {
+    public function getdatatable(){
         $requestData = $_REQUEST;
         $userData = Auth::guard('company')->user();
         $companyId = Company::where('email', $userData->email)->first();
@@ -70,7 +69,71 @@ class Department extends Model
             // datatable column index  => database column name
             0 => 'department.id',
             1 => 'department.department_name',
-            // 2 => 'designation.designation_name',
+            2 => 'designation.designation_name',
+        );
+
+        $query = Department::join('designation', 'designation.department_id', '=', 'department.id')->groupBy('designation.department_id');
+        if (!empty($requestData['search']['value'])) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
+            $searchVal = $requestData['search']['value'];
+            $query->where(function($query) use ($columns, $searchVal, $requestData) {
+                $flag = 0;
+                foreach ($columns as $key => $value) {
+                    $searchVal = $requestData['search']['value'];
+                    if ($requestData['columns'][$key]['searchable'] == 'true') {
+                        if ($flag == 0) {
+                            $query->where($value, 'like', '%' . $searchVal . '%');
+                            $flag = $flag + 1;
+                        } else {
+                            $query->orWhere($value, 'like', '%' . $searchVal . '%');
+                        }
+                    }
+                }
+            });
+        }
+        
+        $temp = $query->orderBy($columns[$requestData['order'][0]['column']], $requestData['order'][0]['dir']);
+        $totalData = count($temp->get());
+        $totalFiltered = count($temp->get());
+        $resultArr = $query->skip($requestData['start'])
+                            ->take($requestData['length'])           
+                            ->select('department.id', 'department.department_name','designation_name',DB::raw('GROUP_CONCAT(designation.designation_name SEPARATOR " | ") AS designation_namea'))
+                            ->get();
+
+        $data = array();
+        foreach ($resultArr as $row) {
+            // print_r($row);exit;
+            $actionHtml ='';
+            $actionHtml .= '<a href="' . route('department-edit', array('id' => $row['id'])) . '" class="link-black text-sm" data-toggle="tooltip" data-original-title="Edit" > <i class="fa fa-edit"></i></a>';
+            $actionHtml .= '<a href="#deleteModel" data-toggle="modal" data-id="'.$row['id'].'" class="link-black text-sm deleteDepartment" data-toggle="tooltip" data-original-title="Delete" > <i class="fa fa-trash"></i></a>';
+            $nestedData = array();
+            $nestedData[] = $row["department_name"];
+            $desigArr = [];
+            foreach ($row->designation as $key => $value) {
+                $desigArr[] = $value["designation_name"];
+            }
+            $nestedData[] = implode(', ', $desigArr);
+            $nestedData[] = '1';
+            $nestedData[] = $actionHtml;
+            $data[] = $nestedData;
+        }
+
+        $json_data = array(
+            "draw" => intval($requestData['draw']), // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
+            "recordsTotal" => intval($totalData), // total number of records
+            "recordsFiltered" => intval($totalFiltered), // total number of records after searching, if there is no searching then totalFiltered = totalData
+            "data" => $data   // total data array
+        );
+        return $json_data;
+    }   
+    public function getdatatableV2(){
+        $requestData = $_REQUEST;
+        $userData = Auth::guard('company')->user();
+        $companyId = Company::where('email', $userData->email)->first();
+        $columns = array(
+            // datatable column index  => database column name
+            0 => 'department.id',
+            1 => 'department.department_name',
+            2 => 'designation.designation_name',
         );
 
         // $query = Department::join('designation', 'designation.department_id', '=', 'department.id');  /*using join*/
