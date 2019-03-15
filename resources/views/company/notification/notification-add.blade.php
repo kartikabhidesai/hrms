668f5d69b81dcd90e4a4782f97f1457635c843d4
shailesh <shailesh.vanaliya91@gmail.com>
2019-03-16 01:03:08 +0530

    change in notification html

--- resources/views/company/notification/notification-add.blade.php
+++ resources/views/company/notification/notification-add.blade.php
@@ -2,28 +2,64 @@
 @section('content')
 <div class="wrapper wrapper-content animated fadeInRight">
     <div class="row">
-        <div class="col-lg-12">
-            {{ csrf_field() }}
-            <div class="ibox float-e-margins">
-                    <div class="ibox-title">
-                        <h5>Sent Notification.</h5>
-                        <div class="ibox-tools">
-                            <!-- <a href="{{ route('add-demo') }}" class="btn btn-primary dim" ><i class="fa fa-plus"> Add</i></a> -->
-                        </div>
-                    </div>
-                    <div class="ibox-content">
-                        <div class="table-responsive">
-                        <table class="table table-striped table-bordered table-hover dataTables-example" id="">
-                            <thead>
-                                <tr>
-                                    <th >Task</th>
-                                    <th>Notify When:</th>
-                                    <th>to all employee</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="gradeU">
+       <div class="col-lg-12">  
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Sent Notification</h5>
+            </div>
+            <div class="ibox-content" style="height: 95px !important;">
+                <div class="form-group">
+                 <div class="col-lg-2">
+                    Department 
+                </div>
+                <div class="col-sm-3">
+                  SMS <input type="checkbox" name=""> <br/> 
+                  Chat <input type="checkbox" name=""><br/> 
+                  In system notify <input type="checkbox" name="">
+              </div> 
+              <div class="col-lg-2">
+                 Purchases <input type="checkbox" name=""> <br/> 
+                 Storage <input type="checkbox" name=""><br/> 
+                 Finance <input type="checkbox" name=""> 
+             </div>
+             <div class="col-sm-3">
+                 Business Deve. <input type="checkbox" name=""> <br/> 
+                 Marketing <input type="checkbox" name=""><br/> 
+                 Event And Planing <input type="checkbox" name="">
+             </div>
+             <div class="col-sm-2">
+              Add More <a href="javascript:;" class="btn btn-xs btn-primary"><i class="fa fa-plus"></i> </a>
+          </div>
+      </div>
+  </div>
+</div>
+</div>
+</div>
+
+<div class="row">
+
+    <div class="col-lg-12">
+        {{ csrf_field() }}
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <h5>Sent Notification List</h5>
+                <div class="ibox-tools">
+                    <!-- <a href="{{ route('add-demo') }}" class="btn btn-primary dim" ><i class="fa fa-plus"> Add</i></a> -->
+                </div>
+            </div>
+            <div class="ibox-content">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover dataTables-example" id="">
+                        <thead>
+                            <tr>
+                                <th >Task</th>
+                                <th>Notify When:</th>
+                                <th>to all employee</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                               <!--  <tr class="gradeU">
                                     <td>Department 
                                         <div class="" style="margin-left: 250px; margin-top: -23px">SMS <input type="checkbox" name=""> <br/> 
                                             Chat <input type="checkbox" name=""><br/> 
@@ -42,8 +78,7 @@
                                     <td>
                                         Add More <a href="javascript:;" class="btn btn-xs btn-primary"><i class="fa fa-plus"></i> </a>
                                     </td>
-                                
-                                </tr>
+                                </tr> -->
                                 <tr class="gradeU">
                                     <td >Inform an employee when new task</td>
                                     <td>SMS <br/> Chat <br/> In system notify</td>
@@ -51,64 +86,64 @@
                                     <td class="center">
                                         <div class="switch">
                                             <div class="onoffswitch">
-                                            <input type="checkbox"  class="onoffswitch-checkbox" id="example1">
+                                                <input type="checkbox"  class="onoffswitch-checkbox" id="example1">
                                                 <label class="onoffswitch-label" for="example1">
                                                     <span class="onoffswitch-inner"></span>
-                                                        <span class="onoffswitch-switch"></span>
-                                                    </label>
-                                                </div>
+                                                    <span class="onoffswitch-switch"></span>
+                                                </label>
                                             </div>
-                                        </td>
+                                        </div>
+                                    </td>
                                 </tr>
                                 <tr class="gradeU">
-                                        <td >Tihs onee is new addred task</td>
-                                        <td>
-                                            SMS <input type="checkbox" name=""> <br/> 
-                                            Chat <input type="checkbox" name=""><br/> 
-                                            In system notify <input type="checkbox" name="">
-                                        </td>
-                                        <td>To all Employee</td>
-                                        <td class="center">
+                                    <td >Tihs onee is new addred task</td>
+                                    <td>
+                                        SMS <input type="checkbox" name=""> <br/> 
+                                        Chat <input type="checkbox" name=""><br/> 
+                                        In system notify <input type="checkbox" name="">
+                                    </td>
+                                    <td>To all Employee</td>
+                                    <td class="center">
                                         <div class="switch">
                                             <div class="onoffswitch">
-                                            <input type="checkbox"  class="onoffswitch-checkbox" id="example2">
+                                                <input type="checkbox"  class="onoffswitch-checkbox" id="example2">
                                                 <label class="onoffswitch-label" for="example2">
                                                     <span class="onoffswitch-inner"></span>
-                                                        <span class="onoffswitch-switch"></span>
-                                                    </label>
-                                                </div>
+                                                    <span class="onoffswitch-switch"></span>
+                                                </label>
                                             </div>
-                                        </td>
-                                    </tr>
-                                    <tr class="gradeU">
-                                        <td >Reports <br/>Inform to employee when leave are accepted.</td>
-                                        <td>
-                                            SMS <input type="checkbox" name=""> <br/> 
-                                            Chat <input type="checkbox" name=""><br/> 
-                                            In system notify <input type="checkbox" name="">
-                                        </td>
-                                        <td>To all Employee</td>
-                                        <td class="center">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="gradeU">
+                                    <td >Reports <br/>Inform to employee when leave are accepted.</td>
+                                    <td>
+                                        SMS <input type="checkbox" name=""> <br/> 
+                                        Chat <input type="checkbox" name=""><br/> 
+                                        In system notify <input type="checkbox" name="">
+                                    </td>
+                                    <td>To all Employee</td>
+                                    <td class="center">
                                         <div class="switch">
                                             <div class="onoffswitch">
-                                            <input type="checkbox"  class="onoffswitch-checkbox" id="example3">
+                                                <input type="checkbox"  class="onoffswitch-checkbox" id="example3">
                                                 <label class="onoffswitch-label" for="example3">
                                                     <span class="onoffswitch-inner"></span>
-                                                        <span class="onoffswitch-switch"></span>
-                                                    </label>
-                                                </div>
+                                                    <span class="onoffswitch-switch"></span>
+                                                </label>
                                             </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
-    <script type="text/javascript">
+</div>
+@endsection
+<script type="text/javascript">
 
-    </script>
+</script>
