361e25bb4330e9ba474537bb7196efe739a89aec
Ravi Gopani <ravigopani555@gmail.com>
2019-03-16 09:42:35 +0530

    performance pdf design updated.

--- resources/views/company/performance/performance-list-pdf.blade.php
+++ resources/views/company/performance/performance-list-pdf.blade.php
@@ -1,77 +1,111 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Pay Slip</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <style>
-            .main-header{
-                font-size: 35px;
-                /*line-height: 35px;
-                text-align: right !important;*/
-            }
-            .text-undeline{
-                text-decoration: underline;
-            }
-            .small-fornt{
-                font-size: 11px;
-                text-align: right;
-            }
-            .page-break {
-                page-break-after: always;
-            }
-            .padding-l-5{
-                padding-left: 5px;
-            }
-        </style>
-        <!-- Favicon -->
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    </head>
-    
-    <body>
-    <div class="invoice-box">
-            <table width="100%">
-                <tr>
-                    <td class="main-header"><span >HRMS</span></td>
-                
-                    <td class="main-header"><span >Performance List</span></td>
-                </tr>
-            </table>
-    </div>
+<style>
+    body
+    {
+      margin: 0mm 0mm 0mm 0mm;
+      font-family: sans-serif;
+    }
+    @page { 
+        margin-top: 0px;
+        margin-left: 10px;
+        margin-right: 10px;
+        margin-bottom: 0px; 
+    }
+    @media print {
+        body
+        {
+          margin: 0px;
+        }
+        .wrap{word-break: break-all;
+        display: inline-block;
+        word-break: break-word;}
+    }
+    table
+    {
+        border-color: #000;
+        color: #000;
+        border-collapse: collapse;
+        font-family: sans-serif;
+    }
+    .table_field{
+        font-size: 14px;
+    }
+    .font-small{
+        font-size: 13px;
+        height: 13px;
+    }
+    .dark{
+        /*color: white;*/
+        /*background-color: #75b979;*/
+        background-color: #ffaa00;
+        font-weight: bold;
+    }
+    .white{
+        color: white;
+    }
+    .light{
+        font-size: 12px;
+        background-color: #DCDCDC;
+        font-weight: bold;
+    }
+    .pfont{
+        font-size: 10px;
+    }
+    * {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    .brdr{
+        border: 2px solid orange;
+    }
+</style>
+
+    <?php
+        $count = sizeof($empPdfArray);
+        // echo "<pre>"; print_r($count); exit();
+        $i = 1; 
+    ?>
+
     @foreach($emparray as $erow)
         @if(!empty($empPdfArray[$erow]))
-        
-        <div class="invoice-box">
-            <table width="100%">
-            
+
+            <table  style="width: 100%; margin:10px;">
                 <tr>
-                    <td >Employee Name</td>
-                    <td >{{ $empPdfArray[$erow][0]['name'] }}</td>                    
-                    <td ></td>
-                    <td >Company Name</td>                    
-                    <td >{{ $empPdfArray[$erow][0]['company_name'] }}</td>
-                </tr> 
+                    <td style="font-size: 25px; font-weight: bold; width: 20%;">
+                        HRMS
+                    </td>
+                    <td style="font-size: 25px; font-weight: bold; width: 60%; text-align: center;">
+                        Performance List
+                    </td>
+                    <td valign="top" style="width: 20%;">
+                    </td>
+                 </tr>
             </table>
-        <br/>
-        <br/>
-            <table width="100%" border="1">
-                <thead>
-                    <tr>
-                        <td>Payments</td>
-                        <td class="padding-l-5">Availability</td>
-                        <td class="padding-l-5">Dependability</td>
-                        <td class="padding-l-5">Job Knowledge</td>
-                        <td class="padding-l-5">Quality</td>
-                        <td class="padding-l-5">Productivity</td>
-                        <td class="padding-l-5">Working <br>Relationship</td>
-                        <td class="padding-l-5">honesty</td>
-                        <td class="padding-l-5">notes_and_details</td>
-                        <td class="padding-l-5">month <br>Year</td>
-                    </tr>
-                </thead>
-                <tbody>
+
+            <table style="width: 100%; margin-top: 10px;" cellpadding="3" border='1'>
+                <tr>
+                    <td class="light table_field" style="width: 20%;">Employee Name : </td>
+                    <td class="font-small" style="width: 30%;">{{ $empPdfArray[$erow][0]['name'] }}</td>
+                    <td class="light table_field" style="width: 20%;">Company Name : </td>
+                    <td class="font-small" style="width: 30%;">{{ $empPdfArray[$erow][0]['company_name'] }}</td>
+                </tr>
+            </table>
+
+            <table style="width: 100%; margin-top: 10px;" cellpadding="3" border='1'>
+                <tr class="light">
+                    <td>Payments</td>
+                    <td>Availability</td>
+                    <td>Dependability</td>
+                    <td>Job Knowledge</td>
+                    <td>Quality</td>
+                    <td>Productivity</td>
+                    <td>Working <br>Relationship</td>
+                    <td>Honesty</td>
+                    <td>Notes & Details</td>
+                    <td>month/Year</td>
+                </tr>
                 @foreach($empPdfArray[$erow] as $employeeArr)
-                    <tr>
+                    <tr class="font-small">
                         <td>{{ $employeeArr['availability'] }}</td>
                         <td>{{ $employeeArr['dependability'] }}</td>
                         <td>{{ $employeeArr['job_knowledge'] }}</td>
@@ -81,14 +115,22 @@
                         <td>{{ $employeeArr['honesty'] }}</td>
                         <td>{{ $employeeArr['notes_and_details'] }}</td>
                         <td>{{ $employeeArr['month'] }}{{ $employeeArr['year'] }}</td>
+                        <td></td>
                     </tr>  
                 @endforeach
-                </tbody>
-            </table>
-            <br>
-        </div>
-        <hr>
-         @endif
-         @endforeach
-    </body>
-</html>
+            </table>       
+            <br/>
+            <hr/>
+
+            <?php $i++;
+                if($i <= $count)
+                {
+            ?>
+                    <div style="page-break-after: always;"></div>
+            <?php
+                }
+            ?>
+            <br/>     
+
+        @endif
+    @endforeach
