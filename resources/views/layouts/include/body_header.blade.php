6f8545559b08208929c79dfdce039e32f943cdb4
shailesh <shailesh.vanaliya91@gmail.com>
2019-03-26 22:34:07 +0530

    add notification link

--- resources/views/layouts/include/body_header.blade.php
+++ resources/views/layouts/include/body_header.blade.php
@@ -73,7 +73,7 @@
                     </a>
                     <ul class="dropdown-menu dropdown-alerts">
                         <li>
-                            <a href="mailbox.html">
+                            <a href="{{ route('notification-list') }}">
                                 <div>
                                     <i class="fa fa-envelope fa-fw"></i> You have 16 messages
                                     <!-- <span class="pull-right text-muted small">4 minutes ago</span> -->
@@ -82,7 +82,7 @@
                         </li>
                         <li class="divider"></li>
                         <li>
-                            <a href="profile.html">
+                            <a href="{{ route('notification-list') }}">
                                 <div>
                                     <i class="fa fa-twitter fa-fw"></i> 3 New Followers
                                     <!-- <span class="pull-right text-muted small">12 minutes ago</span> -->
@@ -91,7 +91,7 @@
                         </li>
                         <li class="divider"></li>
                         <li>
-                            <a href="grid_options.html">
+                            <a href="{{ route('notification-list') }}">
                                 <div>
                                     <i class="fa fa-upload fa-fw"></i> Server Rebooted
                                     <!-- <span class="pull-right text-muted small">4 minutes ago</span> -->
