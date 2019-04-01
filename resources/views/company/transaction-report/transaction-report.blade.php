10c8be54060a18da74483b139f19f3b2bb8b4d64
Hiren Gohel <hirengohel.004@gmail.com>
2019-04-01 23:41:27 +0530

    design UI for transaction report

--- resources/views/company/transaction-report/transaction-report.blade.php
+++ resources/views/company/transaction-report/transaction-report.blade.php
@@ -6,26 +6,67 @@
             {{ csrf_field() }}
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <h5>Manage Transaction Report</h5>
-                    
+                    <h5>Transaction Report</h5>
+                </div>
+                <div class="ibox-content">
+                   {{ Form::open( array('method' => 'post', 'class' => 'form-horizontal', 'id' => 'transactionPDF' )) }}  
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label">Transaction Date</label>
+                            <div class="col-sm-9">
+                                <select class="form-control transaction"  id="transaction" name="transaction">
+                                    <option value="" selected>All Transaction</option>
+                                    <option value="3_months">Last 3 Months Transaction</option>
+                                    <option value="6_months">Last 6 Months Transaction</option>
+                                    <option value="last_year">Last Year Transaction</option>
+                                    <option value="specific_date">Or Choose by Specific Date</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group from_date" style="display: none;">
+                            <label class="col-lg-2 control-label">From Date</label>
+                            <div class="col-sm-9">
+                                <div class="input-group date">
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input type="text" name="from_date" placeholder="Select From Date" class="form-control" value="" autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group to_date" style="display: none;">
+                            <label class="col-lg-2 control-label">To Date</label>
+                            <div class="col-sm-9">
+                                <div class="input-group date">
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input type="text" name="to_date" placeholder="Select To Date" class="form-control" value=""  autocomplete="off">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- <input type="hidden" name="emparray" id="emparray" class="emparray"> -->
+                        <input type="hidden" name="downloadstatus" id="downloadstatus" class="downloadstatus">
+                        <div class="form-group">
+                            <div class="col-lg-offset-2 col-lg-10">
+                                <button class="btn btn-sm btn-primary downloadPdf" type="button">Download Pdf</button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="ibox float-e-margins">
+                <div class="ibox-title">
+                    <h5>Histort of Downloaded Report</h5>
                 </div>
                 <div class="ibox-content">
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered table-hover dataTables-company" >
                             <thead>
                                 <tr>
-                                    <th>Report Title</th>
-                                    
+                                    <th>Number Of Report</th>
+                                    <th>Download Date</th>
                                     <th>Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 
-                            </tbody>
-                            
+                            </tbody>                          
                         </table>
                     </div>
-
                 </div>
             </div>
         </div>
