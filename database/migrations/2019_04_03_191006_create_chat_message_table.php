d106e6662d2b83bcbc34be26d6202d66ba143328
Shivang <prajapati.shiv007@gmail.com>
2019-04-04 00:58:28 +0530

    create two migration table for chat

---
+++ database/migrations/2019_04_03_191006_create_chat_message_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_message', function (Blueprint $table) {
+            $table->increments('message_id');
+            $table->integer('to_user_id');
+            $table->integer('from_user_id');
+            $table->text('chat_message');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_message');
+    }
+}
