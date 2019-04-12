872436728fd03e3f952c9b2c17e41c8c5fad60ed
Chetan <patelchetan1801@gmail.com>
2019-04-13 00:13:52 +0530

    plan mangemnt

---
+++ public/js/admin/planmanage.js
@@ -0,0 +1,70 @@
+var Demo = function() {
+    var handleList = function() {
+     
+        $('body').on('click', '.demoDelete', function() {
+            // $('#deleteModel').modal('show');
+            var id = $(this).data('id');
+            setTimeout(function() {
+                $('.yes-sure:visible').attr('data-id', id);
+            }, 500);
+        })
+
+        $('body').on('click', '.yes-sure', function() {
+            var id = $(this).attr('data-id');
+            var data = {id: id, _token: $('#_token').val()};
+            $.ajax({
+                type: "POST",
+                headers: {
+                    'X-CSRF-TOKEN': $('input[name="_token"]').val(),
+                },
+                url: baseurl + "admin/demo-ajaxAction",
+                data: {'action': 'deleteDemo', 'data': data},
+                success: function(data) {
+                    handleAjaxResponse(data);
+                }
+            });
+        });
+      
+        var form = $('#addDemo');
+        var rules = {
+            first_name: {required: true},
+            last_name: {required: true},
+            demo_pic: {required: true},
+        };
+        handleFormValidate(form, rules, function(form) {
+            handleAjaxFormSubmit(form,true);
+        });
+         var form = $('#editDemo');
+        var rules = {
+            first_name: {required: true},
+            last_name: {required: true},
+            
+        };
+        handleFormValidate(form, rules, function(form) {
+            handleAjaxFormSubmit(form,true);
+        });
+       
+        
+       var dataArr = {};
+       var columnWidth = {"width": "10%", "targets": 0};
+       
+        var arrList = {
+            'tableID': '#dataTables-example',
+            'ajaxURL': baseurl + "admin/demo-ajaxAction",
+            'ajaxAction': 'getdatatable',
+            'postData': dataArr,
+            'hideColumnList': [],
+            'noSearchApply': [0],
+            'noSortingApply': [3,5],
+            'defaultSortColumn': 0,
+            'defaultSortOrder': 'desc',
+            'setColumnWidth': columnWidth
+        };
+        getDataTable(arrList);
+    }
+    return {
+        init: function() {
+            handleList();
+        }
+    }
+}();
