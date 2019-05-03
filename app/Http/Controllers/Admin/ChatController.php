8427e3308133cf4fb8733ffbd108fc8961e7d51c
Pratik Makwana <makwanapratik94@gmail.com>
2019-05-04 00:33:24 +0530

    chat changes

--- app/Http/Controllers/Admin/ChatController.php
+++ app/Http/Controllers/Admin/ChatController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Model\chat;
+use App\Model\Chat;
 // use Illuminate\Support\Facades\Auth;
 use App\Model\Company;
 use App\User;
