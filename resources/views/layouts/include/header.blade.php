409630730f66e230201ab2c6f06554879e9de3c2
Er.Parth Khunt <parthkhunt12@gmail.com>
2019-06-01 17:04:47 +0530

    CHNAGE IN ROLE

--- resources/views/layouts/include/header.blade.php
+++ resources/views/layouts/include/header.blade.php
@@ -2,6 +2,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>HRMS SAAS | Login</title>
 
@@ -16,7 +17,7 @@
     <link href="{{ asset('css/plugins/iCheck/custom.css') }}" rel="stylesheet">
     <link href="{{ asset('css/plugins/fullcalendar/fullcalendar.css') }}" rel="stylesheet">
     <link href="{{ asset('css/plugins/fullcalendar/fullcalendar.print.css') }}" rel="stylesheet" media='print'>
-
+    
     @if (!empty($css)) 
     @foreach ($css as $value) 
     @if(!empty($value))
