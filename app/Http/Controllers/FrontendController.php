f88a9515bdea828e59f3bf0b0e054e0796567d61
Er.Parth Khunt <parthkhunt12@gmail.com>
2019-06-14 15:59:00 +0530

    make frontend

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,23 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-
 use Auth;
 use DB;
 use Session;
 use Redirect;
-use App\Model\AdminUserHasPermission;
-use App\Model\Users;
-use App\Model\Chat;
-use App\Model\Notification;
+
 
 class FrontendController extends Controller {
 
-//    use AuthenticatesUsers;
-//
-//    protected $redirectTo = '/';
     public function __construct() {
-        
     }
     
     public function index(){
