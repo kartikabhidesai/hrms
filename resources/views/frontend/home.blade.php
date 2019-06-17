9064e00c92c16e9dfb0d511267bba9e4efdf94c0
Er.Parth Khunt <parthkhunt12@gmail.com>
2019-06-17 20:38:12 +0530

    chnage in front ends

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -707,71 +707,85 @@
     <!-- Client Section End --> 
 
     <!-- Contact Section Start -->
-    <section id="contact" class="section">      
-      <div class="contact-form">
-        <div class="container">
-          <div class="section-header">          
-            <h2 class="section-title">Get In Touch</h2>
-            <span>Contact</span>
-            <p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
-          </div>
-          <div class="row">          
-            <div class="col-lg-9 col-md-9 col-xs-12">
-              <div class="contact-block">
-<div class="text-center">
-                  <h2>You are using Free Version</h2>
-                  <h4>Please, purchase full version to get all features and pages</h4><br>
-                  <b>Including:</b>
-                  <p>- 4 Homepage Variations</p>
-                  <p>- All Elements and Features</p>
-                  <p>- Amazing Slider</p>
-                  <p>- Documentation File</p>
-                  <p>- Quick Support</p>
-                  <p>- Permission to Use in Commercial Projects</p>
-                  <p>- Footer Credit Removal</p>
-                  <p>- SASS Files</p>
-                  <br>
-                  <a href="https://rebrand.ly/essence-purchase-ud" target="_blank" class="btn btn-common btn-lg">Purchase Now</a>
-</div>
-              </div>
-            </div>
-
-
-
-            <div class="col-lg-3 col-md-3 col-xs-12">
-              <div class="contact-deatils">
-                <!-- Content Info -->
-                <div class="contact-info_area">
-                  <div class="contact-info">
-                    <i class="lni-map"></i>
-                    <h5>Location</h5>
-                    <p>1900 Pico Blvd, New York br Centernial, colorado</p>
-                  </div>
-                  <!-- Content Info -->
-                  <div class="contact-info">
-                    <i class="lni-star"></i>
-                    <h5>E-mail</h5>
-                    <p>info@example.com</p>
-                  </div>
-                  <!-- Content Info -->
-                  <div class="contact-info">
-                    <i class="lni-phone"></i>
-                    <h5>Phone</h5>
-                    <p>+48 123 456 789</p>
-                  </div>
-                  <!-- Icon -->
-                  <ul class="footer-social">
-                    <li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
-                    <li><a class="twitter" href="#"><i class="lni-twitter-filled"></i></a></li>
-                    <li><a class="linkedin" href="#"><i class="lni-linkedin-fill"></i></a></li>
-                    <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
-                  </ul>
+    <section id="contact" class="section">
+        <div class="contact-form">
+            <div class="container">
+                <div class="section-header">
+                    <h2 class="section-title">Get In Touch</h2>
+                        <span>Contact</span>
+                    <p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos debitis.</p>
                 </div>
-              </div>
+                <div class="row">
+                    <div class="col-lg-9 col-md-9 col-xs-12">
+                        <div class="contact-block">
+                            <form id="contactForm">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required data-error="Please enter your name">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" placeholder="Your Email" id="email" class="form-control" name="name" required data-error="Please enter your email">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <input type="text" placeholder="Subject" id="msg_subject" class="form-control" required data-error="Please enter your subject">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea class="form-control" id="message" placeholder="Your Message" rows="7" data-error="Write your message" required></textarea>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                        <div class="submit-button">
+                                            <button class="btn btn-common btn-effect" id="submit" type="submit">Send Message</button>
+                                                <div id="msgSubmit" class="h3 hidden"></div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-xs-12">
+                        <div class="contact-deatils">
+                            <div class="contact-info_area">
+                                <div class="contact-info">
+                                    <i class="lni-map"></i>
+                                    <h5>Location</h5>
+                                    <p>1900 Pico Blvd, New York br Centernial, colorado</p>
+                                </div>
+
+                                <div class="contact-info">
+                                    <i class="lni-star"></i>
+                                    <h5>E-mail</h5>
+                                    <p>info@example.com</p>
+                                </div>
+
+                                <div class="contact-info">
+                                    <i class="lni-phone"></i>
+                                    <h5>Phone</h5>
+                                    <p>+48 123 456 789</p>
+                                </div>
+
+                                <ul class="footer-social">
+                                    <li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
+                                    <li><a class="twitter" href="#"><i class="lni-twitter-filled"></i></a></li>
+                                    <li><a class="linkedin" href="#"><i class="lni-linkedin-fill"></i></a></li>
+                                    <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>            
+        </div> 
     </section>
     <!-- Contact Section End -->
     
@@ -785,7 +799,7 @@
       <section class="footer-Content">
         <div class="container">
           <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6 col-mb-12">
               <h3>Essence</h3>
               <div class="textwidget">
                 <p>If you think you have the passion, 
@@ -800,7 +814,7 @@
                 <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
               </ul> 
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6 col-mb-12">
               <div class="widget">
                 <h3 class="block-title">Short Link</h3>
                 <ul class="menu">
@@ -812,7 +826,7 @@
                 </ul>
               </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6 col-mb-12">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6 col-mb-12">
               <div class="widget">
                 <h3 class="block-title">Contact Us</h3>
                 <ul class="contact-footer">
