aa3dc5cfdd59da4b32305dae97ceb99a7c060aed
kartikdesai123 <kartikdesai123@gmail.com>
2019-09-06 15:00:08 +0530

    feedback

---
+++ database/migrations/2019_09_04_070649_remove_role_to_leave_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveRoleToLeaveCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leave_categories', function (Blueprint $table) {
+           $table->dropColumn('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leave_categories', function (Blueprint $table) {
+            //
+        });
+    }
+}
