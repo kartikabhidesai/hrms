96c58733712355d16d02effd269e46a42831237f
kartik <kartikdesai123@gmail.com>
2019-09-14 20:24:26 +0530

    Solve chat issue

--- resources/views/company/chat/chat.blade.php
+++ resources/views/company/chat/chat.blade.php
@@ -35,7 +35,23 @@
                             No data
                             </div>
                             </div>
-                            
+                            <form method="POST">
+                                <div class="col-md-9" style="padding: 0px;">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="hidden" name="to_user_id"  id="to_user_id" value="">
+                                        <input type="hidden" name="page_no"  id="page_no" value="2">
+                                        <!--<div class="col-lg-12" style="padding-right:0px">-->
+                                            <div class="chat-message-form">
+                                                <div class="form-group">
+                                                    <textarea class="form-control" rows="2" name="message" id="message" placeholder="Enter message text"></textarea>
+                                                </div>
+                                            </div>
+                                        <!--</div>-->
+                                    </div>
+                                    <div class="col-md-3" style="padding: 0px;">
+                                        <a class="btn btn-primary btn-block send_chat" ><i class="fa fa-reply" ></i> Send</a>
+                                    </div>
+                                </form>
                         </div>
                         <div class="col-md-3">
                             <div class="chat-users">
@@ -49,23 +65,7 @@
                         <div class="row">
 
                             
-                                <form method="POST">
-                                    <div class="col-md-9 ">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="hidden" name="to_user_id"  id="to_user_id" value="">
-                                        <input type="hidden" name="page_no"  id="page_no" value="2">
-                                        <div class="col-lg-12" style="padding-right:0px">
-                                            <div class="chat-message-form">
-                                                <div class="form-group">
-                                                    <textarea class="form-control" style="width: 99.2%" rows="2" name="message" id="message" placeholder="Enter message text"></textarea>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-3"  style="margin-left: -37px;width: 27%;">
-                                        <a class="btn btn-primary btn-block send_chat" style="height: 54px;padding-top: 20px"><i class="fa fa-reply" ></i> Send</a>
-                                    </div>
-                                </form>
+                                
                             
                         </div>
                     </div>
@@ -79,5 +79,13 @@
 
     </div>
 </div>
+<style>
+    .send_chat {
+	height: 71px;
+	text-align: center;
+	margin: auto;
+	line-height: 56px;
+}
+</style>
 @endsection
 
