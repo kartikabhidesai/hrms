1ba1419bcbddf9a783db9e85eebc33f794fb6cc3
kartikdesai123 <kartikdesai123@gmail.com>
2019-11-12 18:50:36 +0530

    change

--- app/Model/Notification.php
+++ app/Model/Notification.php
@@ -14,13 +14,13 @@ class Notification extends Model {
 
     protected $table = 'notification';
 
-    public function addNotification($user_id,$message,$route,$notificationMasterId) {
-        
+    public function addNotification($user_id, $message, $route, $notificationMasterId) {
+
         $objNotification = new Notification();
         $objNotification->user_id = $user_id;
         $objNotification->notification_master_id = $notificationMasterId;
-        $objNotification->message =  $message;
-        $objNotification->route =  $route;
+        $objNotification->message = $message;
+        $objNotification->route = $route;
         $objNotification->created_at = date('Y-m-d H:i:s');
         $objNotification->updated_at = date('Y-m-d H:i:s');
         return $objNotification->save();
@@ -35,23 +35,23 @@ class Notification extends Model {
             2 => 'nf.created_at',
         );
         $query = Notification::from('notification as nf')
-                        ->where('nf.user_id',$userid);
+                ->where('nf.user_id', $userid);
         if (!empty($requestData['search']['value'])) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
             $searchVal = $requestData['search']['value'];
             $query->where(function($query) use ($columns, $searchVal, $requestData) {
-                   $flag = 0;
-                   foreach ($columns as $key => $value) {
-                  $searchVal = $requestData['search']['value'];
-                  if ($requestData['columns'][$key]['searchable'] == 'true') {
-                      if ($flag == 0) {
-                          $query->where($value, 'like', '%' . $searchVal . '%');
-                          $flag = $flag + 1;
-                      } else {
-                          $query->orWhere($value, 'like', '%' . $searchVal . '%');
-                      }
-                  }
-                   }
-               });
+                $flag = 0;
+                foreach ($columns as $key => $value) {
+                    $searchVal = $requestData['search']['value'];
+                    if ($requestData['columns'][$key]['searchable'] == 'true') {
+                        if ($flag == 0) {
+                            $query->where($value, 'like', '%' . $searchVal . '%');
+                            $flag = $flag + 1;
+                        } else {
+                            $query->orWhere($value, 'like', '%' . $searchVal . '%');
+                        }
+                    }
+                }
+            });
         }
 
         $temp = $query->orderBy($columns[$requestData['order'][0]['column']], $requestData['order'][0]['dir']);
@@ -60,39 +60,39 @@ class Notification extends Model {
         $totalFiltered = count($temp->get());
 
         $resultArr = $query->skip($requestData['start'])
-                    ->take($requestData['length'])
-                    ->select('nf.id','nf.route', 'nf.message','nf.created_at')->get();
+                        ->take($requestData['length'])
+                        ->select('nf.id', 'nf.route', 'nf.message', 'nf.created_at')->get();
         $data = array();
 
         $objTypeOfRequest = new TypeOfRequest();
         $type_of_request = $objTypeOfRequest->getTypeOfRequestV2($userid);
         // $type_of_request=Config::get('constants.type_of_request');
-     
-        
-        
+
+
+
         foreach ($resultArr as $row) {
             $mystring = $row["route"];
-            $findme   = '/';
+            $findme = '/';
             $pos = strpos($mystring, $findme);
-            if($pos){
-                
-                $routeHtml = '<a style="color:black" href="'.url($row["route"]).'">'.$row["message"].'</a>';
-            }else{
-                $routeHtml = '<a style="color:black" href="'.route($row["route"]).'">'.$row["message"].'</a>';
+            if ($pos) {
+
+                $routeHtml = '<a style="color:black" href="' . url($row["route"]) . '">' . $row["message"] . '</a>';
+            } else {
+                $routeHtml = '<a style="color:black" href="' . route($row["route"]) . '">' . $row["message"] . '</a>';
             }
 //           $actionHtml = $request->input('gender');
-        //    $actionHtml = '<a href="' . route('edit-notification', array('id' => $row['id'])) . '" class="link-black text-sm" data-toggle="tooltip" data-original-title="Edit" > <i class="fa fa-edit"></i></a>';
-            $actionHtml = '<a href="#deleteModel" data-toggle="modal" data-id="'.$row['id'].'" class="link-black text-sm notificationDelete" data-toggle="tooltip" data-original-title="Delete" > <i class="fa fa-trash"></i></a>';
+            //    $actionHtml = '<a href="' . route('edit-notification', array('id' => $row['id'])) . '" class="link-black text-sm" data-toggle="tooltip" data-original-title="Edit" > <i class="fa fa-edit"></i></a>';
+            $actionHtml = '<a href="#deleteModel" data-toggle="modal" data-id="' . $row['id'] . '" class="link-black text-sm notificationDelete" data-toggle="tooltip" data-original-title="Delete" > <i class="fa fa-trash"></i></a>';
             $nestedData = array();
-            $nestedData[] =$row["id"];
-            $nestedData[] =$routeHtml;
-            $nestedData[] = date('d-m-Y',strtotime($row["created_at"]));
-           
-            
+            $nestedData[] = $row["id"];
+            $nestedData[] = $routeHtml;
+            $nestedData[] = date('d-m-Y', strtotime($row["created_at"]));
+
+
             // $nestedData[] = $actionHtml;
             $data[] = $nestedData;
         }
-       // echo "<pre>";print_r($data);exit;
+        // echo "<pre>";print_r($data);exit;
 
         $json_data = array(
             "draw" => intval($requestData['draw']), // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
@@ -103,26 +103,25 @@ class Notification extends Model {
         return $json_data;
     }
 
-
     public function editNotificationStatus($request) {
 
         $objNotification = Notification::find($request->input('user_id'));
-        $objNotification->status =  $request->input('status');
+        $objNotification->status = $request->input('status');
         $objNotification->updated_at = date('Y-m-d H:i:s');
         $objNotification->save();
 
         return TRUE;
     }
 
-    public function zeroNotificationCount($user_id,$notificationID) {
+    public function zeroNotificationCount($user_id, $notificationID) {
         // $notificationid=$this->getNotificationList($user_id);
 
         $objNotification = Notification::find($notificationID);
         // print_r($objNotification);
         // exit;
-                $objNotification->status =  1;
-                $objNotification->updated_at = date('Y-m-d H:i:s');
-                $objNotification->save();
+        $objNotification->status = 1;
+        $objNotification->updated_at = date('Y-m-d H:i:s');
+        $objNotification->save();
 
         // foreach($notificationid as $nid)
         // {
@@ -131,7 +130,6 @@ class Notification extends Model {
         //         $objNotification->updated_at = date('Y-m-d H:i:s');
         //         $objNotification->save();
         // }
-
         // $objNotification = Notification::where('id', $user_id);
         // if (!empty($objNotification)) {
         //     $objNotification->status =  1;
@@ -141,51 +139,51 @@ class Notification extends Model {
 
         return TRUE;
     }
-    
 
     public function getNotificationList($userid) {
-       
+
         $query = Notification::from('notification as notification')
-                ->where('notification.user_id',$userid)
-                ->where('notification.status',0)
-                ->select('notification.id','notification.message','notification.route','notification.created_at')
-                ->orderBy('id','DESC')
-                ->get()->toArray();
+                        ->where('notification.user_id', $userid)
+                        ->where('notification.status', 0)
+                        ->select('notification.id', 'notification.message', 'notification.route', 'notification.created_at')
+                        ->orderBy('id', 'DESC')
+                        ->get()->toArray();
         // $data = array();
-        
+
         return $query;
     }
 
     public function getNotificationCount($userid) {
-       
-        $query = Notification::where('notification.user_id',$userid)
-                            ->where('notification.status',0)
-                            ->get()
-                            ->count();
+
+        $query = Notification::where('notification.user_id', $userid)
+                ->where('notification.status', 0)
+                ->get()
+                ->count();
         return $query;
     }
 
     public function SessionNotificationCount($user_id) {
         $countNotification = $this->getNotificationCount($user_id);
         $notificationList = $this->getNotificationList($user_id);
-        $items[0]['notification_list']=$notificationList;
-        $items[0]['notification_count']=$countNotification;
+        $items[0]['notification_list'] = $notificationList;
+        $items[0]['notification_count'] = $countNotification;
         return $items;
     }
 
     public function getEmployeeNotificationList($userid) {
         $query = DB::table('notification_master as nm')
-                ->select(array('nm.id','nm.notification_name','nm.description'))
-                ->where('nm.key',$type)
+                ->select(array('nm.id', 'nm.notification_name', 'nm.description'))
+                ->where('nm.key', $type)
                 ->get();
         return $query;
     }
-    
-    public function UpdateNotification($userId){
-        $objNotification= DB::table('notification')
-                ->where('user_id',$userId)
-                ->where('status',0)
+
+    public function UpdateNotification($userId) {
+        $objNotification = DB::table('notification')
+                ->where('user_id', $userId)
+                ->where('status', 0)
                 ->update(['status' => 1,
-                          'updated_at' => date('Y-m-d H:i:s')]);
+            'updated_at' => date('Y-m-d H:i:s')]);
     }
+
 }
