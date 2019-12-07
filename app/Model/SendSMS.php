c806f39ef4a878167a7d82b923f38f74c732b592
parthkhunt <parthkhunt12@gmail.com>
2019-12-07 14:34:41 +0530

    change in sendmail

--- app/Model/SendSMS.php
+++ app/Model/SendSMS.php
@@ -208,30 +208,10 @@ class SendSMS extends Model
     }
     
     public function userdetailsmail(){
-       
         $to = "parthkhunt12@gmail.com";
         $subject = "HTML email";
 
-        $message = "
-        <html>
-        <head>
-        <title>HTML email</title>
-        </head>
-        <body>
-        <p>This email contains HTML Tags!</p>
-        <table>
-        <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        </tr>
-        <tr>
-        <td>John</td>
-        <td>Doe</td>
-        </tr>
-        </table>
-        </body>
-        </html>
-        ";
+        $message = "<html><head><title>HTML email</title></head><body><p>This email contains HTML Tags!</p><table><tr><th>Firstname</th><th>Lastname</th></tr><tr><td>John</td><td>Doe</td></tr></table></body></html>";
 
         // Always set content-type when sending HTML email
         $headers = "MIME-Version: 1.0" . "\r\n";
