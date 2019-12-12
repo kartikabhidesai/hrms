54c1b75f7afc1fa088613cdbb1cc8a4f411391f1
parthkhunt <parthkhunt12@gmail.com>
2019-12-12 19:41:07 +0530

    setup

--- app/Model/Chat.php
+++ app/Model/Chat.php
@@ -174,7 +174,8 @@ class Chat extends Model{
         $result = Chat::select('chat_message.*', 'users.name', 'users.user_image')
                     ->join('users', 'chat_message.from_user_id', '=', 'users.id')
                     ->where([['chat_message.to_user_id','=',$toUser],['chat_message.from_user_id','=',$fromUser]])
-                    ->orWhere([['chat_message.from_user_id','=',$toUser],['chat_message.to_user_id','=',$fromUser]])->orderBy('chat_message.created_at', 'DESC')->get();
+                    ->orWhere([['chat_message.from_user_id','=',$toUser],['chat_message.to_user_id','=',$fromUser]])
+                    ->orderBy('chat_message.created_at', 'ASC')->get();
         return $result;
      }
 
@@ -190,7 +191,8 @@ class Chat extends Model{
          $result = Chat::select('chat_message.*', 'users.name', 'users.user_image')
                     ->join('users', 'chat_message.from_user_id', '=', 'users.id')
                     ->where([['chat_message.to_user_id','=',$toUser],['chat_message.from_user_id','=',$fromUser]])
-                    ->orWhere([['chat_message.from_user_id','=',$toUser],['chat_message.to_user_id','=',$fromUser]])->orderBy('chat_message.created_at', 'DESC')->get();
+                    ->orWhere([['chat_message.from_user_id','=',$toUser],['chat_message.to_user_id','=',$fromUser]])
+                    ->orderBy('chat_message.created_at', 'ASC')->get();
         
         return $result;
      }
