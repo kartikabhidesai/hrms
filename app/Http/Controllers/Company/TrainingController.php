2144f318df88f871832af9c4a5a834ca160d50f7
kartikdesai123 <kartikdesai123@gmail.com>
2019-12-13 17:30:26 +0530

    change

--- app/Http/Controllers/Company/TrainingController.php
+++ app/Http/Controllers/Company/TrainingController.php
@@ -18,15 +18,14 @@ use App\Model\Employee;
 use Auth;
 use Route;
 
-class TrainingController extends Controller
-{
+class TrainingController extends Controller {
+
     public function __construct() {
         parent::__construct();
         $this->middleware('company');
     }
 
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         $session = $request->session()->all();
         $data['pluginjs'] = array('jQuery/jquery.validate.min.js');
         $data['js'] = array('company/training.js');
@@ -40,8 +39,7 @@ class TrainingController extends Controller
         return view('company.training.training-list', $data);
     }
 
-    public function addTraining(Request $request)
-    {
+    public function addTraining(Request $request) {
         $session = $request->session()->all();
         $userId = $this->loginUser->id;
         $companyId = Company::select('id')->where('user_id', $userId)->first();
@@ -53,66 +51,64 @@ class TrainingController extends Controller
             $ret = $objCompany->addTraining($request, $companyId->id);
 
             if ($ret) {
-                
+
                 $empId = $request->input('employeeid');
-                $notificationMasterId=7;
+                $notificationMasterId = 7;
                 $objNotificationMaster = new NotificationMaster();
 //                $NotificationUserStatus=$objNotificationMaster->checkNotificationUserStatus($userId,$notificationMasterId);
-                $NotificationUserStatus=$objNotificationMaster->checkNotificationUserStatusNew($userId,$notificationMasterId);
-                    
-                if($NotificationUserStatus->status==1)
-                {
+                $NotificationUserStatus = $objNotificationMaster->checkNotificationUserStatusNew($userId, $notificationMasterId);
+
+                if ($NotificationUserStatus->status == 1) {
                     foreach ($empId as $key => $value) {
-                      
+
                         $objUserNotificationType = new UserNotificationType();
                         $result = $objUserNotificationType->checkMessage($NotificationUserStatus->id);
-       
-                        if($result[0]['status'] == 1){
+
+                        if ($result[0]['status'] == 1) {
 //                            SMS  Notification
-                            $notificationMasterId=7;
-                            $msg= "Company has added a new training.";
+                            $notificationMasterId = 7;
+                            $msg = "Company has added a new training.";
                             $objSendSms = new SendSMS();
-                            $sendSMS = $objSendSms->sendSmsNotificaation($notificationMasterId,$value,$msg);
+                            $sendSMS = $objSendSms->sendSmsNotificaation($notificationMasterId, $value, $msg);
                         }
-                        
-                        if($result[1]['status'] == 1){
+
+                        if ($result[1]['status'] == 1) {
 //                            EMAIL Notification
-                            $notificationMasterId=7;
-                            $msg= "Company has added a new training.";
+                            $notificationMasterId = 7;
+                            $msg = "Company has added a new training.";
                             $objSendEmail = new Users();
-                            $sendEmail = $objSendEmail->sendEmailNotification($notificationMasterId,$value,$msg);
-                            
-                            
+                            $sendEmail = $objSendEmail->sendEmailNotification($notificationMasterId, $value, $msg);
                         }
-                        
-                        if($result[2]['status'] == 1){
+
+                        if ($result[2]['status'] == 1) {
 //                            chat Notification
                         }
-                        
-                        if($result[3]['status'] == 1){
-                        //notification add
+
+                        if ($result[3]['status'] == 1) {
+                            //notification add
                             $objNotification = new Notification();
-                            $trainingName="Company has added a new training.";
+                            $trainingName = "Company has added a new training.";
                             $objEmployee = new Employee();
-                            $u_id=$objEmployee->getUseridById($empId[$key]);
-                            $route_url="employee-training";
-                            $ret = $objNotification->addNotification($u_id,$trainingName,$route_url,$notificationMasterId);
-                        
+                            $u_id = $objEmployee->getUseridById($empId[$key]);
+                            $route_url = "employee-training";
+                            $ret = $objNotification->addNotification($u_id, $trainingName, $route_url, $notificationMasterId);
                         }
                     }
                 }
 
                 $return['status'] = 'success';
                 $return['message'] = 'Training created successfully.';
+                $return['jscode'] = '$(".submitbtn:visible").attr("disabled","disabled");';
                 $return['redirect'] = route('training');
             } else {
                 $return['status'] = 'error';
                 $return['message'] = 'Somethin went wrong while creating new training!';
+                $return['jscode'] ='$(".submitbtn").removeAttr("disabled");';
             }
             echo json_encode($return);
             exit;
         }
-       
+
         $objdepartment = new Department();
         $objDesignation = new Employee();
         $data['department'] = $objdepartment->getDepartment();
--- public/js/company/training.js
+++ public/js/company/training.js
@@ -1,9 +1,9 @@
 var Training = function () {
-    
+
     var handleList = function () {
         var priority = $("#priority").val();
         var status = $("#status").val();
-        var dataArr = {"priority" : priority, "status" : status};
+        var dataArr = {"priority": priority, "status": status};
         var columnWidth = {"width": "10%", "targets": 0};
 
         var arrList = {
@@ -20,32 +20,32 @@ var Training = function () {
         };
         getDataTable(arrList);
 
-        $('body').on('click', '.trainingDelete', function() {
+        $('body').on('click', '.trainingDelete', function () {
             var id = $(this).data('id');
-            setTimeout(function() {
+            setTimeout(function () {
                 $('.yes-sure:visible').attr('data-id', id);
             }, 500);
         })
-        
+
         $('body').on('click', '.filler', function () {
             var priority = $('#priority option:selected').val();
             var status = $('#status option:selected').val();
             var querystring = (priority == '' && typeof priority === 'undefined') ? '&priority=' : '&priority=' + priority;
-            
+
             /*Don't remove this code as it's in-progress*/
             querystring += (status == '' && typeof status === 'undefined') ? '&status=' : '&status=' + status;
-            
+
             location.href = baseurl + 'company/training-list?' + querystring;
-        }); 
-    
-        $('body').on('click', '.deleteTraning', function() {
+        });
+
+        $('body').on('click', '.deleteTraning', function () {
             var id = $(this).data('id');
-            setTimeout(function() {
+            setTimeout(function () {
                 $('.yes-sure:visible').attr('data-id', id);
             }, 500);
         })
- 
-        $('body').on('click', '.yes-sure', function() {
+
+        $('body').on('click', '.yes-sure', function () {
             var id = $(this).attr('data-id');
             var data = {id: id, _token: $('#_token').val()};
             $.ajax({
@@ -55,71 +55,71 @@ var Training = function () {
                 },
                 url: baseurl + "company/training-ajaxAction",
                 data: {'action': 'deleteTraining', 'data': data},
-                success: function(data) {
+                success: function (data) {
                     handleAjaxResponse(data);
                 }
             });
         });
     }
 
-    var addTraining = function(){
-        
-        $('body').on('click','.removeBtn',function(){
-            $(this).parent().parent().remove(); 
+    var addTraining = function () {
+
+        $('body').on('click', '.removeBtn', function () {
+            $(this).parent().parent().remove();
         });
-        
+
         var form = $('#addTraining');
         var rules = {
             location: {required: true},
             budget: {required: true},
             requinment: {required: true},
             department_id: {required: true},
-            numbers: {required: true,number:true},
+            numbers: {required: true, number: true},
             types: {required: true},
         };
-        handleFormValidate(form, rules, function(form) {
-            handleAjaxFormSubmit(form,true);
+        handleFormValidate(form, rules, function (form) {
+            handleAjaxFormSubmit(form, true);
         });
 
-        var empCount=1;
+        var empCount = 1;
         addDepartmentfirst(empCount);
-        
-        function addDepartmentfirst(eid){
-            $("#emp-info").append('<div class="row" id="employee'+eid+'">\
+
+        function addDepartmentfirst(eid) {
+            $("#emp-info").append('<div class="row" id="employee' + eid + '">\
                 <div class="col-lg-1"></div>\
                 <div class="col-lg-3">\
                     <div class="form-group mr-1">\
-                    <select id="Department'+eid+'" data-id="'+eid+'" name="departmentid[]"  class="form-control department"></select>\
+                    <select id="Department' + eid + '" data-id="' + eid + '" name="departmentid[]"  class="form-control department"></select>\
                     </div>\
                 </div>\
                 <div class="col-lg-3">\
                     <div class="form-group mr-1">\
-                    <select id="Employee'+eid+'"  data-id="'+eid+'"  name="employeeid[]" class="form-control employee"></select>\
+                    <select id="Employee' + eid + '"  data-id="' + eid + '"  name="employeeid[]" class="form-control employee"></select>\
                     </div>\
                 </div>\
                 <div class="col-lg-2">\
                 </div>\
             </div>');
 
-             handleEmployee(eid); 
+            handleEmployee(eid);
         }
         // getemployee(empCount);
         $('body').on('click', '.add-emp', function () {
             empCount++;
             addDepartment(empCount);
         });
-        
-        function addDepartment(eid){
-            $("#emp-info").append('<div class="row removediv" id="employee'+eid+'">\
+
+        function addDepartment(eid) {
+            $("#emp-info").append('<div class="row removediv" id="employee' + eid + '">\
                 <div class="col-lg-1"></div>\
                 <div class="col-lg-3">\
                     <div class="form-group mr-1">\
-                    <select id="Department'+eid+'" data-id="'+eid+'" name="departmentid[]"  class="form-control department"></select>\
+                    <select id="Department' + eid + '" data-id="' + eid + '" name="departmentid[]"  class="form-control department"></select>\
                     </div>\
                 </div>\
                 <div class="col-lg-3">\
                     <div class="form-group mr-1">\
-                    <select id="Employee'+eid+'"  data-id="'+eid+'"  name="employeeid[]" class="form-control employee"></select>\
+                    <select id="Employee' + eid + '"  data-id="' + eid + '"  name="employeeid[]" class="form-control employee"></select>\
                     </div>\
                 </div>\
                 <div class="col-lg-2" style="text-align: center;">\
@@ -129,20 +129,20 @@ var Training = function () {
                 </div>\
             </div>');
 
-             handleEmployee(eid);
+            handleEmployee(eid);
         }
 
         $("body").on('click', '.department', function () {
-            var deptVal =  $(this).attr('data-id');
-            if(deptVal > 0){
-                getemployee(deptVal);    
+            var deptVal = $(this).attr('data-id');
+            if (deptVal > 0) {
+                getemployee(deptVal);
             }
         });
-        
-        
-        function handleEmployee(eid ) { 
-           var deptVal =  $('department option:selected').val(); 
-           $.ajax({
+
+
+        function handleEmployee(eid) {
+            var deptVal = $('department option:selected').val();
+            $.ajax({
                 type: "POST",
                 headers: {
                     'X-CSRF-TOKEN': $('input[name="_token"]').val(),
@@ -154,47 +154,49 @@ var Training = function () {
                     var len = output.length;
 //                     console.log(output);
 //                     exit;
-                        $("#Department"+eid).empty();
-                        var temphtml="<option value=''>Select Department</option>";
-                        for( var i = 0; i<len; i++){
-                            var id = output[i]['id'];
-                            var name = output[i]['department_name'];
-                            var temp ="<option value='"+id+"'>"+name+"</option>";
-                            temphtml = temphtml + temp;
-                        }
-                        $("#Department"+eid).append(temphtml);
-                        // $("#Department"+eid).on("change", getemployee(eid));
-                        setTimeout(function(){ 
-                            getemployee(eid);
-                        }, 1000);
+                    $("#Department" + eid).empty();
+                    var temphtml = "<option value=''>Select Department</option>";
+                    for (var i = 0; i < len; i++) {
+                        var id = output[i]['id'];
+                        var name = output[i]['department_name'];
+                        var temp = "<option value='" + id + "'>" + name + "</option>";
+                        temphtml = temphtml + temp;
+                    }
+                    $("#Department" + eid).append(temphtml);
+                    // $("#Department"+eid).on("change", getemployee(eid));
+                    setTimeout(function () {
+                        getemployee(eid);
+                    }, 1000);
                 }
             });
-        } 
-        function getemployee(eid) {   
-           
-            var department = $('#Department'+eid).val();
-           
+        }
+        function getemployee(eid) {
+
+            var department = $('#Department' + eid).val();
+
             $.ajax({
                 type: "POST",
                 headers: {
                     'X-CSRF-TOKEN': $('input[name="_token"]').val(),
                 },
                 url: baseurl + "company/employee-ajaxAction",
-                data: {'action': 'getDepartmentEmployeeList','department_id':department},
+                data: {'action': 'getDepartmentEmployeeList', 'department_id': department},
                 success: function (data) {
                     var output = JSON.parse(data);
                     console.log(output)
                     var len = output.length;
-                    $("#Employee"+empCount).empty();
-                    $("#Employee"+empCount).append("<option value=''>Select Employee</option>");
-                    for( var i = 0; i<len; i++){
+                    $("#Employee" + empCount).empty();
+                    $("#Employee" + empCount).append("<option value=''>Select Employee</option>");
+                    for (var i = 0; i < len; i++) {
                         var id = output[i]['id'];
                         var name = output[i]['name'];
-                        $("#Employee"+empCount).append("<option value='"+id+"'>"+name+"</option>");
+                        $("#Employee" + empCount).append("<option value='" + id + "'>" + name + "</option>");
                     }
                 }
             });
         }
+
+        
 
     }
 
@@ -202,8 +204,8 @@ var Training = function () {
         init: function () {
             handleList();
         },
-        add:function(){
-          addTraining();  
+        add: function () {
+            addTraining();
         },
     }
 }();
